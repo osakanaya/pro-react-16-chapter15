06ce1bfd7981c26cff5bf15abb4b5e86fab75f1d
sashida <sashida@primagest.co.jp>
2020-10-10 10:16:23 +0900

    p.432

--- src/Editor.js
+++ src/Editor.js
@@ -4,12 +4,22 @@ export class Editor extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            name: ""
+            name: "Bob",
+            flavor: "Vanilla",
+            toppings: ["Strawberries"]
         };
+
+        this.flavors = ["Chocolate", "Double Chocolate", "Triple Chocolate", "Vanilla"];
+        this.toppings = ["Sprinkles", "Fudge Sauce", "Strawberries", "Maple Syrup"];
     }
 
     updateFormValue = (event) => {
         this.setState({ [event.target.name]: event.target.value }, () => this.props.submit(this.state));
+    }
+
+    updateFormValueOptions = (event) => {
+        let options = [...event.target.options].filter(o => o.selected).map(o => o.value);
+        this.setState({ [event.target.name]: options }, () => this.props.submit(this.state));
     }
 
     render() {
@@ -17,6 +27,30 @@ export class Editor extends Component {
             <div className="form-group">
                 <label>Name</label>
                 <input className="form-control" name="name" value={ this.state.name } onChange={ this.updateFormValue } />
+            </div>
+            
+            <div className="form-group">
+                <label>Ice Cream Flavor</label>
+                <select className="form-control" name="flavor" value={ this.state.flavor }
+                    onChange={ this.updateFormValue } >
+                    { this.flavors.map(flavor => 
+                        <option value={ flavor } key={ flavor }>
+                            { flavor }
+                        </option>
+                    )}
+                </select>
+            </div>
+
+            <div className="form-group">
+            <label>Ice Cream Toppings</label>
+                <select className="form-control" multiple={ true } name="toppings" value={ this.state.toppings }
+                    onChange={ this.updateFormValueOptions } >
+                    { this.toppings.map(top => 
+                        <option value={ top } key={ top }>
+                            { top }
+                        </option>
+                    )}
+                </select>
             </div>
         </div>
     }
