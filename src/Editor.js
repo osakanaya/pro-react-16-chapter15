f8f243978d8d4b4bba5f1bac2bd191dbcad31dab
sashida <sashida@primagest.co.jp>
2020-10-10 10:47:05 +0900

    p.4379

--- src/Editor.js
+++ src/Editor.js
@@ -5,8 +5,7 @@ export class Editor extends Component {
         super(props);
         this.state = {
             name: "Bob",
-            flavor: "Vanilla",
-            toppings: ["Strawberries"]
+            order: ""
         };
 
         this.flavors = ["Chocolate", "Double Chocolate", "Triple Chocolate", "Vanilla"];
@@ -17,37 +16,18 @@ export class Editor extends Component {
         this.setState({ [event.target.name]: event.target.value }, () => this.props.submit(this.state));
     }
 
-    updateFormValueCheck = (event) => {
-        event.persist();
-        
-        this.setState(state => {
-            if (event.target.checked) {
-                state.toppings.push(event.target.name);
-            } else {
-                let index = state.toppings.indexOf(event.target.name);
-                state.toppings.splice(index, 1);
-            }
-            }, () => this.props.submit(this.state));
-    }
-
     render() {
         return <div className="h5 bg-info text-white p-2">
             <div className="form-group">
                 <label>Name</label>
                 <input className="form-control" name="name" value={ this.state.name } onChange={ this.updateFormValue } />
             </div>
-            
+
             <div className="form-group">
-                <label>Ice Cream Toppings</label>
-                { this.toppings.map(top =>
-                    <div className="form-check" key={ top }>
-                        <input className="form-check-input" type="checkbox" name={ top }
-                            value={ top } checked={ this.state.toppings.indexOf(top) > -1 }
-                            onChange={ this.updateFormValueCheck } />
-                        <label className="form-check-label">{ top }</label>
-                    </div>
-                )}
-           </div>
+                <label>Order</label>
+                <textarea className="form-control" name="order" value={ this.state.order }
+                    onChange={ this.updateFormValue } />
+            </div>            
         </div>
     }
 }
