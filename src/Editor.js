0407357a8bbd75ae4152e6bfc70a7b1e078cbebe
sashida <sashida@primagest.co.jp>
2020-10-10 10:08:39 +0900

    p.429

--- src/Editor.js
+++ src/Editor.js
@@ -1,9 +1,23 @@
 import React, { Component } from "react";
 
 export class Editor extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: ""
+        };
+    }
+
+    updateFormValue = (event) => {
+        this.setState({ [event.target.name]: event.target.value }, () => this.props.submit(this.state));
+    }
+
     render() {
         return <div className="h5 bg-info text-white p-2">
-            Form Will  Go Here
+            <div className="form-group">
+                <label>Name</label>
+                <input className="form-control" name="name" value={ this.state.name } onChange={ this.updateFormValue } />
+            </div>
         </div>
     }
 }
