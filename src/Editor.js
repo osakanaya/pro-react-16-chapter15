a5fafaa9042efddd5f2669707ac66c061e81d723
sashida <sashida@primagest.co.jp>
2020-10-20 09:00:45 +0900

    p.456

--- src/Editor.js
+++ src/Editor.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { FormValidator } from "./FormValidator";
 import { ValidationMessage } from "./ValidationMessage";
+import { ValidateForm } from "./whioleFormValidation";
 
 export class Editor extends Component {
     constructor(props) {
@@ -28,7 +29,11 @@ export class Editor extends Component {
 
     render() {
         return <div className="h5 bg-info text-white p-2">
-            <FormValidator data={ this.state } rules={ this.rules } submit={ this.props.submit }>
+            <FormValidator data={ this.state } rules={ this.rules } submit={ this.props.submit }
+                validateForm={ ValidateForm } >
+
+                <ValidationMessage field="form" />
+                
                 <div className="form-group">
                     <label>Name</label>
                     <input className="form-control" name="name" value={ this.state.name } 
--- src/FormValidator.js
+++ src/FormValidator.js
@@ -14,9 +14,15 @@ export class FormValidator extends Component {
     }
 
     static getDerivedStateFromProps(props, state) {
-        return {
-            errors: ValidateData(props.data, props.rules)
-        };
+        state.errors = ValidateData(props.data, props.rules);
+        if (state.formSubmitted && Object.keys(state.errors).length === 0) {
+            let formErrors = props.validateForm(props.data);
+            if (formErrors.length > 0) {
+                state.errors.form = formErrors;
+            }
+        }
+
+        return state;
     }
 
     get formValid() {
@@ -31,7 +37,10 @@ export class FormValidator extends Component {
     handleClick = (ev) => {
         this.setState({ formSubmitted: true }, () => {
             if (this.formValid) {
-                this.props.submit(this.props.data);
+                let formErrors = this.props.validateForm(this.props.data);
+                if (formErrors.length === 0) {
+                    this.props.submit(this.props.data);
+                }
             }
         });
     }
